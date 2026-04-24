4b2c95ba64b26f93c539a3bce9f4926c5ef1b8be
Fahsai-Nattapan Tammawongsa <nattapan.t@7solutions.co.th>
2026-04-25 02:04:21 +0700

    feat: create Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Hero from "./components/Hero";
 import Menu from "./components/Menu/Menu";
+
 function App() {
   return (
-    <div className="min-h-screen overflow-hidden">
+    <div className="min-h-screen flex flex-col">
       <Menu />
+      <main className="flex-1">
+        <Hero />
+      </main>
     </div>
   );
 }
+
 export default App;
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,27 @@
+function Hero() {
+  return (
+    <section className="px-6 pt-16 md:pt-44">
+      <div className="mx-auto max-w-4xl text-center">
+        <h1 className="text-4xl font-semibold leading-tight text-gray-900 md:text-5xl">
+          Make Your First <span className="text-blue-600">Impression</span>{" "}
+          Count
+        </h1>
+
+        <p className="mx-auto mt-6 max-w-md text-base leading-relaxed text-gray-500 sm:max-w-md  md:max-w-xl md:text-lg">
+          powerful self-serve product analytics to help you convert engage and
+          retain more users
+        </p>
+
+        <div className="mt-8 flex items-center justify-center gap-4">
+          <button className="rounded-xl bg-blue-600 px-6 py-4 text-base font-semibold text-white hover:bg-blue-700 sm:text-lg">
+            Get Started
+          </button>
+          <button className="rounded-xl border border-gray-300 px-6 py-4 text-base font-semibold text-gray-500 hover:bg-gray-50 sm:text-lg">
+            Watch Demo
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Hero;
