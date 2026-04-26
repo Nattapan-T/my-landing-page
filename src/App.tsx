1991be9f4a0a47fb727f0475bcb6508f7c25e3f9
Fahsai-Nattapan Tammawongsa <nattapan.t@7solutions.co.th>
2026-04-26 09:59:36 +0700

    fix: edit import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Menu from "./components/menu";
-import Hero from "./components/hero";
-import Client from "./components/client";
-import Feature from "./components/feature";
-import RequestDemo from "./components/request-demo";
-import Showcase from "./components/showcase";
-import GrowTogether from "./components/grow-together";
-import FAQ from "./components/faq";
-import PreFooter from "./components/prefooter";
-import Footer from "./components/footer";
+import Menu from "./components/menu/index.tsx";
+import Hero from "./components/hero/index.tsx";
+import Client from "./components/client/index.tsx";
+import Feature from "./components/feature/index.tsx";
+import RequestDemo from "./components/request-demo/index.tsx";
+import Showcase from "./components/showcase/index.tsx";
+import GrowTogether from "./components/grow-together/index.tsx";
+import FAQ from "./components/faq/index.tsx";
+import PreFooter from "./components/prefooter/index.tsx";
+import Footer from "./components/footer/index.tsx";
 import chartImage from "./assets/77bed93c22cc94457124aff6df696718c0c77f3c.png";
 
 function App() {
